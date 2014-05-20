1cf55398f79c0d67b0f67b711a5a90518655bbb5
Arnold Visser <arnold@uwebshop.com>
2014-05-20 10:51:31 +0200

    Small Update and updated ignore file

--- uWebshop.Shipping.Example/ShippingExample.cs
+++ uWebshop.Shipping.Example/ShippingExample.cs
@@ -1,65 +1,94 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using uWebshop.Common;
+using uWebshop.API;
 using uWebshop.Domain;
-using uWebshop.Domain.Helpers;
 using uWebshop.Domain.Interfaces;
+using Store = uWebshop.API.Store;
 
 namespace uWebshop.Shipping.Example
 {
-	public class ExampleShippingProvider : IShippingProvider
-	{
-		private readonly IHttpRequestSender _requestSender;
+    public class ExampleProvider : IShippingProvider
+    {
+        private readonly IHttpRequestSender _requestSender;
 
-		public ExampleShippingProvider()
-		{
-			_requestSender = new HttpRequestSender();
-		}
+        public ExampleProvider()
+        {
+            _requestSender = new HttpRequestSender();
+        }
 
-		public ExampleShippingProvider(IHttpRequestSender requestSender = null)
-		{
-			_requestSender = requestSender ?? new HttpRequestSender();
-		}
+        public ExampleProvider(IHttpRequestSender requestSender = null)
+        {
+            _requestSender = requestSender ?? new HttpRequestSender();
+        }
 
-		public string GetName()
-		{
-			return "Example";
-		}
+        public string GetName()
+        {
+            return "Example";
+        }
 
-		public IEnumerable<ShippingProviderMethod> GetAllShippingMethods(int id)
-		{
-			//var provider = new ShippingProvider(id);
-			//var helper = new ShippingConfigHelper(provider);
-			//var request = new ShippingRequest();
-			//var orderInfo = OrderHelper.GetOrderInfo();
+        public IEnumerable<ShippingProviderMethod> GetAllShippingMethods(int id)
+        {
+            //var provider = new ShippingProvider(id);
+            //var helper = new ShippingConfigHelper(provider);
+            //var request = new ShippingRequest();
+            //var orderInfo = OrderHelper.GetOrderInfo();
 
-			string issuerId = "0";
-			string issuerName = "DHL";
+            var vat = Store.GetStore().GlobalVat;
 
-			var method = new ShippingProviderMethod {ShippingProviderUpdateService = new ExampleShippingProviderShippingProviderUpdateService(), Id = issuerId, Description = issuerName, Title = issuerName, Name = issuerName, ProviderName = GetName()};
+            var order = Basket.GetBasket();
 
-			return new List<ShippingProviderMethod> {method};
-		}
+            if (order != null)
+            {
+                vat = order.AverageOrderVatPercentage;
+            }
 
-		public class ExampleShippingProviderShippingProviderUpdateService : IShippingProviderUpdateService
-		{
-			public void Update(ShippingProviderMethod shippingProviderMethod, OrderInfo orderInfo)
-			{
-				Log.Instance.LogDebug("Hoeveelheid artikelen totaal: " + orderInfo.OrderLines.Sum(x => x.ProductInfo.ItemCount));
-				Log.Instance.LogDebug("Hoeveel eerste artikel in mandje: " + orderInfo.OrderLines.First().ProductInfo.ItemCount);
+            var issuerId = "0";
+            var issuerName = "Example";
 
-				// CALCULATE SHIPPING PRICE
+            var method = new ShippingProviderMethod
+            {
+                ShippingProviderUpdateService =
+                    new ExampleShippingProviderShippingProviderUpdateService(),
+                Id = issuerId,
+                Description = issuerName,
+                Title = issuerName,
+                Name = issuerName,
+                ProviderName = GetName(),
+                Vat = vat,
+                PriceInCents = 500
+            };
 
-				int priceInCents;
-				var itemCount = orderInfo.OrderLines.Sum(line => line.ProductInfo.ItemCount.GetValueOrDefault(1));
-				if (itemCount == 1)
-					priceInCents = 500;
-				else if (itemCount == 2)
-					priceInCents = 400;
-				else
-					priceInCents = 0;
-				shippingProviderMethod.PriceInCents = priceInCents;
-			}
-		}
-	}
+            return new List<ShippingProviderMethod> { method };
+        }
+        public class ExampleShippingProviderShippingProviderUpdateService : IShippingProviderUpdateService
+        {
+            public void Update(ShippingProviderMethod shippingProviderMethod, OrderInfo orderInfo)
+            {
+                if (orderInfo != null)
+                {
+                    // your logic here, and set the price at the end like this:
+
+                    Log.Instance.LogDebug("Hoeveelheid artikelen totaal: " +
+                                          orderInfo.OrderLines.Sum(x => x.ProductInfo.ItemCount));
+                    Log.Instance.LogDebug("Hoeveel eerste artikel in mandje: " +
+                                          orderInfo.OrderLines.First().ProductInfo.ItemCount);
+
+                    // CALCULATE SHIPPING PRICE
+
+                    int priceInCents;
+                    var itemCount = orderInfo.OrderLines.Sum(line => line.ProductInfo.ItemCount.GetValueOrDefault(1));
+                    if (itemCount == 1)
+                        priceInCents = 500;
+                    else if (itemCount == 2)
+                        priceInCents = 400;
+                    else
+                        priceInCents = 0;
+                    shippingProviderMethod.PriceInCents = priceInCents;
+
+                    shippingProviderMethod.PriceInCents = 500;
+                }
+            }
+        }
+    }
 }
